a7af322108e3936151a60e7dbc0c93e7ee469c1e
duarte.mati@gmail.com <duarte.mati@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-03-25 14:41:45 +0000

    Agrego la figura faltante

--- src/codigo/TP taller/TP taller/Modelo/Rectangulo.cpp
+++ src/codigo/TP taller/TP taller/Modelo/Rectangulo.cpp
@@ -1,8 +1,24 @@
 #include "Rectangulo.h"
 
 
-Rectangulo::Rectangulo(void)
+Rectangulo::Rectangulo()
 {
+	//DETERMINAR VALORES POR DEFECTO
+}
+
+Rectangulo::Rectangulo(float x, float y, short int rotacion, b2World* world, bool estatico, float ancho, float alto, float masa)
+	: Figura(x , y , rotacion, world, estatico)
+{
+	b2PolygonShape rectanguloShape;
+	//Divido a la mitad el ancho y alto para que las medidas sean correctas
+	rectanguloShape.SetAsBox(ancho/2, alto/2);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &rectanguloShape;
+	float areaRec = ancho * alto;
+	fixtureDef.density = masa/areaRec;
+	this->getBody()->CreateFixture(&fixtureDef);
+
 }
 
 
