24e5f66c700af690aa7a96df9e5fdeaf1c0e54b8
marianknot92@gmail.com <marianknot92@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-03-26 06:42:30 +0000

    terminada la mascara, subo un par de terrenos aleatorios para tener por si acaso

--- trunk/src/codigo/TP taller/TP taller/constantes.h
+++ trunk/src/codigo/TP taller/TP taller/constantes.h
@@ -2,22 +2,22 @@
 #define CONSTANTES_H_
 
 	#define archivoLog "logErrors.txt"
-	#define imagenTerrenoDEF "imagenes/terrenoDefault.txt" //para que se usa?? (el tererno default se genera con el nombre del YAML)
+	#define imagenTerrenoDEF "imagenes/terrenoDefault.png"
 	#define imagenCieloDEF "imagenes/cieloDefault.txt"
 	#define configPath "config/config.yaml"
 
 	const double PI = 3.1415926535;
 
 	const unsigned anchoPxDEF=3000;
-	const unsigned altoPxDEF=768; //tiene q usar el parser para que le asigne el alto del escenario
+	const unsigned altoPxDEF=768;
 	const unsigned anchoUDEF=3000;
 	const unsigned altoUDEF=1000;
 	const unsigned nivelAguaDEF=20;
 
 	//cuando la imagen de terreno es invalida, se genera una con una f(x) aleatoria
 	//estos parametros ajustan el valor minimo y maximo de la funcion
-	const int fMinDEFAULT = 20;
-	const int fMaxDEFAULT = 400;
+	const int fMinDEFAULT = 10;
+	const int fMaxDEFAULT = 500;
 
 	#define DEGTORAD 0.0174532925199432957f
 	#define RADTODEG 57.295779513082320876f
