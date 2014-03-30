a81ee6f3504a631ef1ed1c3b214303935e46ea9f
quimey.funes@gmail.com <quimey.funes@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-03-30 01:24:01 +0000

    -agregada clase Juego

---
+++ trunk/src/codigo/TP taller/TP taller/Parser/ParserDeHexARgb.cpp
@@ -0,0 +1,20 @@
+#include "ParserDeHexARgb.h"
+
+using namespace std;
+
+/*
+SDL usa RGB, y llega hex del yaml. 
+Asumo que me llega bien el string: "#XXXXXX" , X de 0 a F
+*/
+int* ParserDeHexARgb::parsearDeHexARgb(string hex) {
+	string aux;
+	int* rgb = new int[3];
+	hex.erase(0,1);
+	for (int i = 0; i<3; i++) {
+		aux.append(hex.begin()+i*2,hex.begin()+(i+1)*2);
+		rgb[i] =  strtol(aux.c_str(),NULL,16);
+		aux = "";
+
+	}
+	return rgb;
+}
