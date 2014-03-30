b85f32f337e5c525735789e7bc4448ab03c0bf14
quimey.funes@gmail.com <quimey.funes@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-03-30 03:02:18 +0000

    -pequeño cambio

--- trunk/src/codigo/TP taller/TP taller/Parser/ParserDeHexARgb.cpp
+++ trunk/src/codigo/TP taller/TP taller/Parser/ParserDeHexARgb.cpp
@@ -9,7 +9,6 @@ Asumo que me llega bien el string: "#XXXXXX" , X de 0 a F
 int* ParserDeHexARgb::parsearDeHexARgb(string hex) {
 	string aux;
 	int* rgb = new int[3];
-	hex.erase(0,1);
 	for (int i = 0; i<3; i++) {
 		aux.append(hex.begin()+i*2,hex.begin()+(i+1)*2);
 		rgb[i] =  strtol(aux.c_str(),NULL,16);
