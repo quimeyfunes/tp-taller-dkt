5eb6017198565e7551c411c26ad3584407b8ee00
quimey.funes@gmail.com <quimey.funes@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-04-11 03:15:36 +0000

    -el cliente ya se conecta al servidor

--- branches/segundaentrega/src/codigo/Servidor/TP taller/main.cpp
+++ branches/segundaentrega/src/codigo/Servidor/TP taller/main.cpp
@@ -1,7 +1,25 @@
+#include "Modelo/Red/Servidor.h"
+#include "Modelo/Red/Cliente.h"
 #include "Modelo/Juego/Juego.h"
+#include <process.h>
+
+void servidorLoop(void *);
+void clienteLoop(void);
+
+Servidor * server;
+Cliente * client;
 
 int main(int argc, char* argv[]){	
-	if(argc == 2){
+
+	// initialize the server
+    server = new Servidor();
+	// create thread with arbitrary argument for the run function
+    _beginthread( servidorLoop, 0, (void*)12);
+
+    // initialize the client 
+    client = new Cliente();
+	clienteLoop();
+	/*if(argc == 2){
 	ParserYaml::setConfigPath(argv[1]);
 	}
 	try{
@@ -13,5 +31,24 @@ int main(int argc, char* argv[]){
 		Logger::getLogger()->escribir(e.what());
 		Logger::getLogger()->guardarEstado();
 	}
-	return 0;    
+	return 0; */   
+	return 0;
 }
+
+void servidorLoop(void * arg) 
+{ 
+    while(true) 
+    {
+        server->actualizar();
+    }
+}
+
+void clienteLoop()
+{
+    while(true)
+    {
+        //do game stuff
+       //will later run client->update();
+    }
+}
+
