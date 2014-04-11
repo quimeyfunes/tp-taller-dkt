d37cfbace3118c0422466b2d19ab9c5a37010dfd
quimey.funes@gmail.com <quimey.funes@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-04-11 23:32:34 +0000

    -agrega parte de envio de informacion entre cliente y servidor

--- src/codigo/Servidor/TP taller/Modelo/Red/Cliente.cpp
+++ src/codigo/Servidor/TP taller/Modelo/Red/Cliente.cpp
@@ -3,4 +3,15 @@
 Cliente::Cliente(void)
 {
     red = new ClienteRed();
+
+	 // send init packet
+    const unsigned int paquete_tamano = sizeof(Paquete);
+    char paquete_data[paquete_tamano];
+
+    Paquete paquete;
+    paquete.tipoPaquete = INIT_CONNECTION;
+
+    paquete.serializar(paquete_data);
+
+	Servicio::enviarMensaje(red->socketCliente, paquete_data, paquete_tamano);
 }
