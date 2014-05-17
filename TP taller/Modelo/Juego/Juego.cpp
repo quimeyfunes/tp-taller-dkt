9599a6aac0309c5fd5783dff1ce833dbdc1beffa
franery10@gmail.com <franery10@gmail.com@2bf23e61-47ca-9e4b-0456-3e5a69972b03>
2014-05-17 16:58:20 +0000

    Destruccion terreno. 3ra entrega

---
+++ TP taller/Modelo/Juego/Juego.cpp
@@ -0,0 +1,265 @@
+	#include "Juego.h"
+
+Servidor* Juego::servidor = NULL;
+
+Juego::Juego(){
+}
+
+Juego::Juego(string texto){
+	this->simulando = true;
+	this->estadoActual = JUGANDO;
+	this->evento = new SDL_Event();
+	ParserYaml* parser = ParserYaml::getParser();
+	EscenarioParseado* e = parser->getEscenario();
+	this->vista = new Vista(e);
+	//SDL_HideWindow(this->vista->window);
+	this->escenario = new Escenario(e->altoU ,e->anchoU, e->nivelAgua, relacionPPU, relacionPPU, e->maximosClientes);
+	this->terreno = new Terreno(this->escenario->getWorld());
+	this->terreno->generarTerreno(e->imagenTierra);
+	this->escenario->setTerreno(this->terreno);
+	this->mundo = escenario->getWorld();
+	ResolverContacto* resolverContacto = new ResolverContacto();
+	this->mundo->SetContactListener(resolverContacto);
+	agregarTexturas(e);
+	agregarObjetos();
+	agregarAgua(e);
+}
+
+
+void Juego::ejecutar(){
+	Logger::getLogger()->guardarEstado();
+
+	servidor = new Servidor();
+	 int tamanio;
+
+	//game loop
+	while(this->estadoActual != SALIDA && (evento->type != SDL_QUIT)){
+		
+		this->chequearNuevosJugadores();
+		this->leerEvento();
+
+		if(simulando){
+			switch(estadoActual){
+
+				case JUGANDO:		jugar();	break;
+				case PAUSADO:		esperar();	break;
+			}
+		}
+		escenario->notificar();
+		this->servidor->dibujablesSerializados = this->crearLista(tamanio);
+		this->vista->Dibujar();
+		SDL_Delay(3);
+
+	}
+}
+
+string Juego::crearLista(int &tamanio){
+	//list<DibujableSerializado> lista;
+	string lista = "";
+	string serializado = "";
+	tamanio=0;
+	bool first = true;
+	for (list<Dibujable*>::iterator it = vista->getListaDibujables()->begin(); it != vista->getListaDibujables()->end(); it++) {
+		serializado = (*it)->serializar();
+		if(serializado != ""){
+			if(!first){
+				lista += separadorEntidades;
+			}
+			lista += serializado;
+			first = false;
+		}
+	}
+
+	return lista;
+}
+
+void Juego::chequearNuevosJugadores(){
+	for(int i=0; i< this->escenario->getMaximosClientes(); i++){
+		if(this->servidor->clientes[i].activo){
+			if(this->servidor->clientes[i].figuras.size() == 0){
+				//Si el cliente esta activo y no tiene figuras es porque acaba de conectarse. Le asigno gusanos
+				for(int j=0; j< gusanosPorPersonaje; j++){
+					float escalaAncho = relacionPPU;
+					float escalaAlto = relacionPPU;
+					Gusano* worm = escenario->crearGusanoParaJugador();
+					if (worm){
+						string nombreGusano = this->servidor->clientes[i].username + " "+StringUtil::int2string(j);
+						GusanoSprite* gusano = vista->crearGusanoSprite( worm->getPosicion().x * escalaAncho, worm->getPosicion().y * escalaAlto , anchoGusano * 5, altoGusano * 5, spriteWormIzq, 1, 10, 60, 600,nombreGusano,this->escenario->getMaximosClientes());
+						worm->agregarObservador(gusano);
+						this->servidor->clientes[i].figuras.push_back(worm);
+					} 
+					this->escenario->inicializarCliente(i);
+				}
+			}
+		}
+	}
+}
+
+void Juego::leerEvento(){
+	 if (this->vista->leerEvento(evento)){
+
+                switch(this->vista->getAccion()){
+
+                case SALIR:                     salir();                                                break;
+                case JUGAR:                     reiniciar();                                    break;
+                case PAUSAR:            alternarPausa();                                break;
+                case ARRIBA:            this->escenario->arriba(true);          break;
+                case IZQUIERDA:         this->escenario->izquierda(true);       break;
+                case DERECHA:           this->escenario->derecha(true);         break; 
+                case SOLTARARRIBA:              this->escenario->arriba(false);         break;
+                case SOLTARIZQUIERDA:   {this->escenario->izquierda(false);
+                                                                 this->escenario->reiniciarTeclas();}   break;
+                case SOLTARDERECHA:             {this->escenario->derecha(false);
+                                                                 this->escenario->reiniciarTeclas();}   break; 
+                case CLICK:     
+                        int x,y;
+                        SDL_GetMouseState(&x,&y);
+                        if (!(this->escenario->click((x + this->vista->getCorrimientoX()) / (relacionPPU * this->vista->getZoom()) ,  (y + this->vista->getCorrimientoY()) / (relacionPPU * this->vista->getZoom())))) {
+							this->vista->destruir(x,y,40 * relacionPPU);
+						}
+                        break;
+                }
+       }
+		
+	
+	
+	//Lector de eventos de los clientes. Lo anterior lo dejo para que siga funcionando mover en el servidor
+	for(int i=0; i< this->escenario->getMaximosClientes(); i++){
+		if(this->servidor->clientes[i].activo){
+			//Si el cliente esta activo chequeo eventos
+			string ultimoEvento = this->servidor->clientes[i].ultimoEventoSerializado;
+			if(ultimoEvento != ""){
+				Evento* evento = new Evento();
+				evento->deserealizar(ultimoEvento);
+				switch(evento->accion){
+
+					case SALIR:			salir();						break;
+					case JUGAR:			reiniciar();					break;
+					case PAUSAR:		alternarPausa();				break;
+					case ARRIBA:		this->escenario->arribaCliente(i,true);		break;
+					case IZQUIERDA:		this->escenario->izquierdaCliente(i,true);	break;
+					case DERECHA:		this->escenario->derechaCliente(i,true);		break; 
+					case SOLTARARRIBA:		this->escenario->arribaCliente(i,false);		break;
+					case SOLTARIZQUIERDA:	{this->escenario->izquierdaCliente(i,false);
+											 this->escenario->reiniciarTeclas();}	break;
+					case SOLTARDERECHA:		{this->escenario->derechaCliente(i,false);
+											 this->escenario->reiniciarTeclas();}	break; 
+					case CLICK:	
+						list<Gusano*> figurasOtrosClientes;
+						for(int j=0; j< this->escenario->getMaximosClientes(); j++){
+							if(i != j){
+								figurasOtrosClientes.insert(figurasOtrosClientes.end(), this->servidor->clientes[j].figuras.begin(), this->servidor->clientes[j].figuras.end());
+							}
+						}
+						this->escenario->clickCliente(i,this->servidor->clientes[i].figuras,figurasOtrosClientes, evento->x, evento->y);
+						break;
+				}
+
+				delete evento;
+				this->servidor->clientes[i].ultimoEventoSerializado = "";
+			}
+		}
+	}
+
+}
+
+void Juego::jugar(){
+	mundo->Step(stepTiempo, iteracionesVelocidad, iteracionesPosicion);
+	escenario->simularAgua();
+}
+
+void Juego::salir(){
+	this->estadoActual = SALIDA;
+}
+
+void Juego::reiniciar(){
+	this->simulando = !simulando;
+	this->escenario->reiniciar();
+
+	if(this->estadoActual == PAUSADO) this->estadoActual = JUGANDO;
+}
+
+void Juego::alternarPausa(){
+	if(this->estadoActual != PAUSADO) this->estadoActual = PAUSADO;	else this->estadoActual = JUGANDO;
+}
+
+void Juego::esperar(){}
+
+void Juego::agregarTexturas(EscenarioParseado* e){
+
+	vista->crearDibujableTextura(0, 0, e->anchoU*relacionPPU, e->altoU*relacionPPU, texturaFondo, "");
+	vista->crearDibujableTextura(0, 0, e->anchoU*relacionPPU, e->altoU*relacionPPU, e->imagenCielo, texturaCieloDEF);
+	vista->crearScrollingSprite(0, 10,  e->anchoPx/ 5, e->altoPx /10, rutaNube1);
+	vista->crearScrollingSprite( e->anchoU*relacionPPU/2, 30, e->anchoPx / 5, e->altoPx / 10, rutaNube2);
+	vista->crearDibujableTextura(0, 0, e->anchoU*relacionPPU, e->altoU*relacionPPU,terreno->getLector()->getRutaTexturaActualizada(), "");
+}
+
+void Juego::agregarAgua(EscenarioParseado* e){
+	//vista->crearDibujableTextura(0, e->nivelAgua*this->escenario->getRelacionAlto(), terreno->getLector()->getAnchoMatriz(), terreno->getLector()->getAltoMatriz() - e->nivelAgua*this->escenario->getRelacionAlto(), texturaAgua, texturaAguaDEF);
+	
+	for(int i=0;i<4;i++){
+		//vista->crearSprite( (i* terreno->getLector()->getAnchoMatriz())/4, e->nivelAgua* this->escenario->getRelacionAlto() - 15,  terreno->getLector()->getAnchoMatriz()/4, 15, spriteOlas, 2, 6, 256, 144);
+		Sprite* sprite = vista->crearSprite( (i* e->anchoU*relacionPPU)/4, e->nivelAgua*relacionPPU - 24,  e->anchoU*relacionPPU/4, 24, spriteOlas, 2, 6, 256, 144);
+		sprite->setTransparencia(230);
+	}
+	DibujableTextura* agua = vista->crearDibujableTextura(0, e->nivelAgua*relacionPPU, e->anchoU*relacionPPU, (e->altoU - e->nivelAgua) *relacionPPU, texturaAgua, texturaAguaDEF);
+	agua->setTransparencia(230);
+}
+
+void Juego::agregarObjetos(){
+	Gusano* worm;
+	vector<ObjetoParseado>* objetos = ParserYaml::getParser()->getObjetos();	
+	EscenarioParseado* e = ParserYaml::getParser()->getEscenario();
+
+	float escalaAncho = relacionPPU;
+	float escalaAlto = relacionPPU;
+	for (std::vector<ObjetoParseado>::iterator it = objetos->begin(); it != objetos->end(); ++it) {
+		switch ((*it).tipo) {
+		case 1: 
+			{
+				Circulo* cir = escenario->crearCirculo(*it);
+				if (cir) {
+					CirculoDibujable* circulo = vista->crearCirculoDibujable((*it).x * escalaAncho, (*it).y * escalaAlto,(*it).escala * escalaAncho,(*it).escala* escalaAlto);
+					circulo->setColor(ParserDeHexARgb::parsearDeHexARgb((*it).color));
+					cir->agregarObservador(circulo);
+				}
+				break;
+			}
+		case 2:
+			{
+				/*Rectangulo* rec = escenario->crearRectangulo(*it);
+				if(rec){
+					//cout<<(*it).ancho<<endl;
+					RectanguloDibujable* rectangulo = vista->crearRectanguloDibujable((*it).ancho * escalaAncho, (*it).alto * escalaAlto);
+					rectangulo->setColor(ParserDeHexARgb::parsearDeHexARgb((*it).color));
+					rec->agregarObservador(rectangulo);
+				}
+				break;
+			}*/
+				worm = escenario->crearGusano(*it);
+				if (worm){
+					//GusanoDibujable* gusano = vista->crearGusanoDibujable((*it).x * escalaAncho, (*it).y * escalaAlto , (*it).ancho * escalaAncho, (*it).alto * escalaAlto, rutaGusano, rutaGusanoDEF);
+					GusanoSprite* gusano = vista->crearGusanoSprite( (*it).x * escalaAncho, (*it).y * escalaAlto , anchoGusano * 5, altoGusano * 5, spriteWormIzq, 1, 10, 60, 600,"Rasta",4);
+					worm->agregarObservador(gusano);
+				} 
+				break;
+			}
+		default:
+			{
+				Poligono* pol = escenario->crearPoligono(*it);
+				if (pol) {
+					PoligonoDibujable* poligono = vista->crearPoligonoDibujable((*it).tipo,(*it).escala * escalaAncho, (*it).escala * escalaAlto);
+					poligono->setColor(ParserDeHexARgb::parsearDeHexARgb((*it).color));
+					pol->agregarObservador(poligono);
+				}
+				break;
+			}
+		}
+	}
+}
+
+Juego::~Juego(){
+
+	//delete this->evento;
+	//delete Logger::getLogger();
+}
---
+++ TP taller/Vista/Vista.cpp
@@ -0,0 +1,320 @@
+#include "Vista.h"
+
+
+Vista::Vista(EscenarioParseado* e){
+	SDL_Init( SDL_INIT_EVERYTHING );
+	this->anchoPx = e->anchoPx;
+	this->altoPx = e->altoPx;
+	this->anchoPxTot = e->anchoU * relacionPPU;
+	this->altoPxTot = e->altoU * relacionPPU;
+	this->window = SDL_CreateWindow("Worms!", 25, 25, e->anchoPx, e->altoPx,  SDL_WINDOW_SHOWN);
+	this->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	this->listaDibujables = new list<Dibujable*>;
+
+	this->corrimientoX = 0;
+	this->corrimientoY = 0;
+	SDL_SetWindowIcon(this->window, IMG_Load(rutaIcono));
+	this->accion = JUGAR;
+	this->setZoom(escalaZoomDefault);
+	TTF_Init();
+}
+
+Vista::Vista() {
+}
+
+Vista::~Vista() {
+
+	delete this->window;
+	delete this->renderer;
+	delete this->listaDibujables;
+	SDL_Quit();
+	TTF_Quit();
+}
+
+SDL_Renderer* Vista::getRenderer() {
+	return this->renderer;
+}
+
+void Vista::crearScrollingSprite(int x, int y, int ancho, int alto, string path){
+
+	SDL_Rect rec;
+	rec.x = x;
+	rec.y = y;
+	rec.w = ancho;
+	rec.h = alto;
+	ScrollingSprite* sprite = new ScrollingSprite(this->renderer, rec, path);
+	this->agregarDibujable(sprite);	
+}
+
+Sprite* Vista::crearSprite(int x, int y, int anchoFrame, int altoFrame, string path, int col, int fil, int anchoTex, int altoTex){
+
+	SDL_Rect recFrame;
+	recFrame.x = x;
+	recFrame.y = y;
+	recFrame.w = anchoFrame;
+	recFrame.h = altoFrame;
+	Sprite* sprite = new Sprite(this->renderer, recFrame, path, col, fil, anchoTex, altoTex);
+	this->agregarDibujable(sprite);	
+	return sprite;
+}
+
+GusanoSprite* Vista::crearGusanoSprite(int x, int y, int anchoFrame, int altoFrame, string path, int col, int fil, int anchoTex, int altoTex,string nombre,int maximosClientes){
+
+	SDL_Rect recFrame;
+	recFrame.x = x;
+	recFrame.y = y;
+	recFrame.w = anchoFrame;
+	recFrame.h = altoFrame;
+	//Hardcodeo el nombre, nose si se va a seguir usando igual esto
+	GusanoSprite* gusano = new GusanoSprite(this->renderer, recFrame, path, col, fil, anchoTex, altoTex, nombre,maximosClientes);
+	this->agregarDibujable(gusano);	
+	return gusano;
+}
+
+CartelInfo* Vista::crearCartelInfo(int x, int y, int ancho, int alto){
+	SDL_Rect rec;
+	rec.x=x;
+	rec.y=y;
+	rec.h=alto;
+	rec.w=ancho;
+
+	CartelInfo* cartelInfo = new CartelInfo(this->renderer, rec);
+	this->agregarDibujable(cartelInfo);
+	return cartelInfo;
+}
+
+DibujableTextura* Vista::crearDibujableTextura(int x , int y ,int ancho,int alto, string pathImagen, string imagenDEF) {
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	rect.w = ancho;
+	rect.h = alto;
+	
+	DibujableTextura* dib = new DibujableTextura(this->renderer, rect, pathImagen, imagenDEF);
+	this->agregarDibujable(dib);
+	return dib;
+}
+
+CirculoDibujable* Vista::crearCirculoDibujable(int x , int y ,int radioHorizontal, int radioVertical) {
+	CirculoDibujable* dib = new CirculoDibujable(x,y,radioHorizontal,radioVertical);
+	this->agregarDibujable(dib);
+	return dib;
+}
+
+PoligonoDibujable* Vista::crearPoligonoDibujable(short int n, float escalaAncho, float escalaAlto) {
+	PoligonoDibujable* dib = new PoligonoDibujable(n,escalaAncho,escalaAlto);
+	this->agregarDibujable(dib);
+	return dib;
+}
+
+RectanguloDibujable* Vista::crearRectanguloDibujable(float ancho, float alto) {
+	RectanguloDibujable* dib = new RectanguloDibujable(ancho,alto);
+	this->agregarDibujable(dib);
+	return dib;
+}
+
+GusanoDibujable* Vista::crearGusanoDibujable(int x, int y, int ancho, int alto, string pathImagen, string imagenDEF){
+
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	rect.w = ancho;
+	rect.h = alto;
+	
+	GusanoDibujable* dib = new GusanoDibujable(this->renderer, rect,pathImagen,imagenDEF);
+	CartelDibujable* dib2 = dib->getCartel();
+
+	/*SDL_Rect rectCart = dib2->getRect();
+	rectCart.x = rect.x;
+	rectCart.y = rect.y + 80;
+	rectCart.w = rect.w;
+	rectCart.h = rect.h / 4;*/
+
+	this->agregarDibujable(dib);
+	this->agregarDibujable(dib2);
+	return dib;
+
+}
+
+
+void Vista::agregarDibujable(Dibujable* dibujable) {
+	this->listaDibujables->push_back(dibujable);
+}
+
+list<Dibujable*>* Vista::getListaDibujables(){
+	return this->listaDibujables;
+}
+
+void Vista::setListaDibujables(list<Dibujable*>* dibujables){
+	this->listaDibujables = dibujables;
+}
+
+void Vista::Dibujar(){
+	SDL_RenderClear(this->renderer);
+	for (list<Dibujable*>::iterator it = this->listaDibujables->begin(); it != this->listaDibujables->end(); it++) {
+		(*it)->dibujar(this->renderer, this->corrimientoX, this->corrimientoY, this->escalaZoom, this->anchoPx, this->altoPx);
+	}
+	SDL_RenderPresent(this->renderer);
+}
+
+int Vista::getAnchoPx() {
+	return this->anchoPx;
+}
+
+int Vista::getAltoPx() {
+	return this->altoPx;
+}
+
+bool Vista::leerEvento(SDL_Event* evento) {
+	int x,y;
+	SDL_GetMouseState(&x,&y);
+	this->scroll(x,y);
+	if (SDL_PollEvent(evento) != 0) {
+
+		if(evento->type == SDL_QUIT){
+			this->accion = SALIR;
+			return true;
+		}
+		
+		if(evento->type == SDL_KEYDOWN){
+
+			switch(evento->key.keysym.sym){
+
+			case SDLK_ESCAPE:	this->accion = SALIR;		return true;	break;
+			case SDLK_s:		this->accion = JUGAR;		return true;	break;
+			case SDLK_p:		this->accion = PAUSAR;		return true;	break;
+			case SDLK_UP:		this->accion = ARRIBA;		return true;	break;
+			case SDLK_LEFT:		this->accion = IZQUIERDA;	return true;	break;
+			case SDLK_RIGHT:	this->accion = DERECHA;		return true;	break; 
+
+			}
+			
+		} else {
+			if (evento->type == SDL_KEYUP) {
+				switch(evento->key.keysym.sym){
+					case SDLK_UP:		this->accion = SOLTARARRIBA;		return true;	break;
+					case SDLK_LEFT:		this->accion = SOLTARIZQUIERDA;		return true;	break;
+					case SDLK_RIGHT:	this->accion = SOLTARDERECHA;		return true;	break; 
+				}
+			}
+		}
+
+		if (evento->type == SDL_MOUSEBUTTONDOWN) {
+			this->accion = CLICK;
+			return true;
+		}
+
+		if (evento->type == SDL_MOUSEWHEEL){
+			this->zoom(evento,x,y);
+		}
+	}
+	
+	return false;
+}
+
+ACCION_REALIZADA Vista::getAccion(){
+	return this->accion;
+}
+
+float Vista::getCorrimientoX(){
+	return this->corrimientoX;
+}
+
+float Vista::getCorrimientoY(){
+	return this->corrimientoY;
+}
+
+float Vista::getZoom(){
+	return this->escalaZoom;
+}
+
+void Vista::setZoom(float escala){
+	if (escala > zoomMax) {
+		this->escalaZoom = zoomMax;
+		return;
+	} 
+	float zoomMinAncho = this->anchoPx * 1.0 / this->anchoPxTot;
+	float zoomMinAlto = this->altoPx * 1.0 / this->altoPxTot;
+	if ((zoomMinAlto > zoomMinAncho) && (escala < zoomMinAlto)){
+		this->escalaZoom = zoomMinAlto;
+		return;
+	}
+	if ((zoomMinAlto <= zoomMinAncho) && (escala < zoomMinAncho)){
+		this->escalaZoom = zoomMinAncho;
+		return;
+	}
+	// Cuando pasa a zoomMin termina con un numero (generalmente) no multiplo de 0.25. Aca lo vuelvo a multiplo de 0.25. 
+	int esc = escala * 100;
+	if (esc % 25 != 0) { 
+		this->escalaZoom = (esc / 25) * 0.25;
+		return;
+	}
+	this->escalaZoom = escala;
+	
+}
+
+void Vista::scroll(int x , int y) {
+	if ((x < (this->anchoPx * porcentajeScroll)) && ( x != 0)) {
+		this->corrimientoX -= this->anchoPx * velocidadScroll / x ;
+	} else {
+		if ((x > (this->anchoPx * (1 - porcentajeScroll))) && ( x < (this->anchoPx - 1))) {
+			this->corrimientoX += this->anchoPx * velocidadScroll / (this->anchoPx - x);
+		} 
+	}
+	if ((y < (this->altoPx * porcentajeScroll)) && ( y != 0)) {
+		this->corrimientoY -= this->altoPx * velocidadScroll / y ; 
+	} else {
+		if ((y > (this->altoPx * (1 - porcentajeScroll))) && ( y < (this->altoPx - 1))) {
+			this->corrimientoY += this->altoPx * velocidadScroll / (this->altoPx - y);
+		} 
+	}
+	this->validarCorrimiento();
+}
+
+void Vista::zoom(SDL_Event* evento,int x, int y) {
+
+	float escalaAnterior = this->escalaZoom;
+		
+	if (evento->wheel.y > 0) {
+		this->setZoom(this->escalaZoom + 0.25);
+	} else {
+		this->setZoom(this->escalaZoom - 0.25);
+	}
+
+	if (this->escalaZoom != escalaAnterior) {
+		this->corrimientoX = (this->corrimientoX + x ) / escalaAnterior;
+		this->corrimientoY = (this->corrimientoY + y ) / escalaAnterior;
+		this->corrimientoX = (this->corrimientoX * escalaZoom) - this->anchoPx / 2;
+		this->corrimientoY = (this->corrimientoY * escalaZoom)  - this->altoPx / 2;
+	}
+	this->validarCorrimiento();
+}
+
+void Vista::validarCorrimiento() {
+	if ((this->corrimientoX + this->anchoPx) > this->anchoPxTot * escalaZoom) {
+		this->corrimientoX = this->anchoPxTot * escalaZoom - this->anchoPx;
+	} else {
+		if (this->corrimientoX < 0) {
+			this->corrimientoX = 0;
+		}
+	}
+	if ((this->corrimientoY + this->altoPx) > this->altoPxTot * escalaZoom) {
+		this->corrimientoY = this->altoPxTot * escalaZoom- this->altoPx;
+	} else {
+		if (this->corrimientoY < 0) {
+			this->corrimientoY = 0;
+		}
+	}
+}
+
+void Vista::destruir(float x,float y,int radio){
+	int i = 0;
+	/*for (list<Dibujable*>::iterator it = this->listaDibujables->begin(); it != this->listaDibujables->end() && i < 6; it++) {
+		if ( i== 5) {
+			DibujableTextura* d = (DibujableTextura*) (*it);
+			d->getImagen()
+
+		}
+		i++;
+	}*/
+}
